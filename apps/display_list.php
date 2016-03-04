0b499c94604237c9f6f06f34ad7abc6c0740dc52
thomasbatt <thomasbatt@gmail.fr>
2016-03-04 12:27:53 +0100

    maj correction des erreurs pascal

--- apps/display_list.php
+++ apps/display_list.php
@@ -1,5 +1,4 @@
 <?php
-
 $statut = mysqli_real_escape_string($db, $statut);
 $query = "SELECT 
 			ticket_tickets.id AS ticket_id, title, ticket_tickets.statut AS ticket_statut, content, ticket_tickets.date AS ticket_date, dead_line, img AS url_img, treatment_id, ticket_tickets.editing,
@@ -8,50 +7,33 @@ $query = "SELECT
 			LEFT JOIN ticket_users ON ticket_tickets.user_id = ticket_users.id 
 			WHERE ticket_tickets.statut = '".$statut."'  
 			ORDER BY ticket_tickets.id DESC";
-
 $res_user = mysqli_query($db, $query);
-
 while ($ticket_user = mysqli_fetch_assoc($res_user))
 {
-	$tiket = [
+	$ticket = [
 		'id' => $ticket_user['ticket_id'],
 		'title' => $ticket_user['title'],
-		'ticket_statut' => $ticket_user['ticket_statut'],
+		'statut' => $ticket_user['ticket_statut'],
 		'content' => $ticket_user['content'],
-		'ticket_date' => $ticket_user['ticket_date'],
+		'date' => $ticket_user['ticket_date'],
 		'dead_line' => $ticket_user['dead_line'],
 		'url_img' => $ticket_user['url_img'],
 		'treatment_id' => $ticket_user['treatment_id'],
 		'editing' => $ticket_user['editing']
 	];
-
-
-	$ticket_id = $ticket_user['ticket_id'];
-	$title = $ticket_user['title'];
-	$ticket_statut = $ticket_user['ticket_statut'];
-	$content = $ticket_user['content'];
-	$ticket_date = $ticket_user['ticket_date'];
-	$dead_line = $ticket_user['dead_line'];
-	$url_img = $ticket_user['url_img'];
-	$treatment_id = $ticket_user['treatment_id'];
-	$editing = $ticket_user['editing'];
-
-	$user_id = $ticket_user['user_id'];
-	$url_avatar = $ticket_user['url_avatar'];
-	$first_name = $ticket_user['first_name'];
-	$last_name = $ticket_user['last_name'];
-	$login = $ticket_user['login'];
-	$phone = $ticket_user['phone'];
-	$user_statut = $ticket_user['user_statut'];
-	/* ##PASCAL ~> Pareil ici pas besoin de faire 15 variables... c'est un peu crade, faites une variable $user et une variable $ticket, c'est plus propre ! */
-	// var_dump($ticket_user);
-	
-	if ($editing)
+	$user = [
+		'id' => $ticket_user['user_id'],
+		'url_avatar' => $ticket_user['url_avatar'],
+		'first_name' => $ticket_user['first_name'],
+		'last_name' => $ticket_user['last_name'],
+		'login' => $ticket_user['login'],
+		'phone' => $ticket_user['phone'],
+		'statut' => $ticket_user['user_statut']
+	];
+	if ($ticket['editing'])
 		$ticket_file = 'ticket_edit'; 
 	else
 		$ticket_file = 'ticket'; 
-
 	require('views/'.$ticket_file.'.phtml');
-
 }
 ?>
--- apps/display_ticket.php
+++ apps/display_ticket.php
@@ -2,47 +2,43 @@
 if (isset($_GET['id']))
 {
 	$id = intval($_GET['id']);
-	/* ##PASCAL ~> Requete oksauf que as est un mot clef, c'est mieux si vous l'écrivez en majuscules > AS */
 	$query = "SELECT 
-				ticket_tickets.id as ticket_id, title, ticket_tickets.statut as ticket_statut, content, ticket_tickets.date as ticket_date, dead_line, img as url_img, treatment_id, ticket_tickets.editing,
-				ticket_users.id as user_id, ticket_users.avatar as url_avatar, ticket_users.first_name, ticket_users.last_name, ticket_users.login, ticket_users.phone, ticket_users.statut as user_statut
+				ticket_tickets.id AS ticket_id, title, ticket_tickets.statut AS ticket_statut, content, ticket_tickets.date AS ticket_date, dead_line, img AS url_img, treatment_id, ticket_tickets.editing,
+				ticket_users.id AS user_id, ticket_users.avatar AS url_avatar, ticket_users.first_name, ticket_users.last_name, ticket_users.login, ticket_users.phone, ticket_users.statut AS user_statut
 				FROM ticket_tickets 
 				LEFT JOIN ticket_users ON ticket_tickets.user_id = ticket_users.id 
 				WHERE ticket_tickets.id = '".$id."'  
 				ORDER BY ticket_tickets.id DESC";
-
 	$res_user = mysqli_query($db, $query);
-	$statut = 'todo';
+	$ticket['statut'] = 'todo';
 	$editing = true;
 	while ($ticket_user = mysqli_fetch_assoc($res_user))
 	{
-		$ticket_id = $ticket_user['ticket_id'];
-		$title = $ticket_user['title'];
-		$ticket_statut = $ticket_user['ticket_statut'];
-		$content = $ticket_user['content'];
-		$ticket_date = $ticket_user['ticket_date'];
-		$dead_line = $ticket_user['dead_line'];
-		$url_img = $ticket_user['url_img'];
-		$treatment_id = $ticket_user['treatment_id'];
-		// $editing = $ticket_user['editing'];
-
-		$user_id = $ticket_user['user_id'];
-		$url_avatar = $ticket_user['url_avatar'];
-		$first_name = $ticket_user['first_name'];
-		$last_name = $ticket_user['last_name'];
-		$login = $ticket_user['login'];
-		$phone = $ticket_user['phone'];
-		$user_statut = $ticket_user['user_statut'];
-		/* ##PASCAL ~> Pareil ici pas besoin de faire 15 variables... c'est un peu crade, faites une variable $user et une variable $ticket, c'est plus propre ! */
-		// var_dump($ticket_user);
-		
+		$ticket = [
+			'id' => $ticket_user['ticket_id'],
+			'title' => $ticket_user['title'],
+			'statut' => $ticket_user['ticket_statut'],
+			'content' => $ticket_user['content'],
+			'date' => $ticket_user['ticket_date'],
+			'dead_line' => $ticket_user['dead_line'],
+			'url_img' => $ticket_user['url_img'],
+			'treatment_id' => $ticket_user['treatment_id'],
+			// 'editing' => $ticket_user['editing']
+		];
+		$user = [
+			'id' => $ticket_user['user_id'],
+			'url_avatar' => $ticket_user['url_avatar'],
+			'first_name' => $ticket_user['first_name'],
+			'last_name' => $ticket_user['last_name'],
+			'login' => $ticket_user['login'],
+			'phone' => $ticket_user['phone'],
+			'statut' => $ticket_user['user_statut']
+		];
 		if ($editing)
 			$ticket_file = 'ticket_edit'; 
 		else
 			$ticket_file = 'ticket'; 
-
 		require('views/'.$ticket_file.'.phtml');
-
 	}
 }
 ?>
