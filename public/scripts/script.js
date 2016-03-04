b0b59b52c454ee011f979bf9896ad48e00492986
GregoryJT <interfacile@gmail.com>
2016-03-04 12:32:58 +0100

    MAJ drag and drop coldone

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -18,9 +18,27 @@ $('document').ready(function()
 });
 
 // script jqueryUI sortable
+
+// deplacement de todo vers current sans retour en arriere possible
 $(function() {
     $("#coltodo, #colcurrent").sortable({
       connectWith: "#colcurrent",
+      stop:function(event, ui)
+      {
+      	var id = $(ui.item).find('[name="ticket_id"]').val();
+      	var statut = $(ui.item).find('[name="statut"]').val();
+      	$.post('index.php', {ticket_id:id,statut:statut,action:'next_ticket'}, function()
+  		{
+  			
+  		});
+      }
+    }).disableSelection();
+  });
+
+// deplacement de current vers done sans retour en arriere possible
+$(function() {
+    $("#colcurrent, #coldone").sortable({
+      connectWith: "#coldone",
       stop:function(event, ui)
       {
       	var id = $(ui.item).find('[name="ticket_id"]').val();
